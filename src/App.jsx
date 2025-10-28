8319c19b93e4e46328a657aad0cd5e34efc90eb5
code-bit-source <amritmr760@gmail.com>
2025-10-28 16:39:33 +0530

    versel json

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/HomePage/Home'
 import Aboutus from './pages/aboutus/Aboutus'
 import Contact from './pages/contact/Contact'
